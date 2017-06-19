ff723ea02f2cb2bed0b403cb09b93fa1ed809d55
Lary81 <lary81@interia.pl>
2017-06-19 15:31:07 +0200

    feefwgb

---
+++ sages/bootcamp/git/GitHello.java
@@ -0,0 +1,12 @@
+package sages.bootcamp.git;
+
+/**
+ * Created by Sysia on 2017-06-19.
+ */
+public class GitHello {
+
+    public static void main(String[] args) {
+        System.out.println ("Hello git!");
+    }
+
+}
